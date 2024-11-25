81c227463ef1b6940b2f4efb66ae298edde037af
Won <trilliwon@gmail.com>
2024-11-25 16:30:27 +0900

    Bump to 0.2.10

--- release.sh
+++ release.sh
@@ -16,20 +16,26 @@ if [[ ! $VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   exit 1
 fi
 
-# Validate the version number in DaroAds.podspec
-if ! grep -q "spec.version *= *'$VERSION'" DaroMAds.podspec; then
-  echo "Version $VERSION does not match the version in DaroMAds.podspec"
-  exit 1
-fi
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PODSPEC_PATH="$SCRIPT_DIR/DaroMAds.podspec"
+
+# Update version in podspec file
+sed -i '' "s/spec\.version = '[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}'/spec.version = '$VERSION'/" "$PODSPEC_PATH"
+
+# Commit and push the changes
+git add .
+git commit -m "Bump version to $VERSION"
+git push origin main
 
 # Check if DaroM.xcframework.zip exists
-if [ ! -f "build/DaroM.xcframework.zip" ]; then
-  echo "build/DaroM.xcframework.zip file not found"
+if [ ! -f "$SCRIPT_DIR/build/DaroM.xcframework.zip" ]; then
+  echo "$SCRIPT_DIR/build/DaroM.xcframework.zip file not found"
   exit 1
 fi
 
 # Create a tag and release using GitHub CLI with the file
-gh release create $VERSION "build/DaroM.xcframework.zip" --title "Release $VERSION" --notes "Release version $VERSION"
+gh release create $VERSION "$SCRIPT_DIR/build/DaroM.xcframework.zip" --title "Release $VERSION" --notes "Release version $VERSION"
+git pull origin main
 
 # Push the podspec to the trunk
-pod trunk push DaroMAds.podspec --allow-warnings --verbose
+pod trunk push "$PODSPEC_PATH" --allow-warnings --verbose
